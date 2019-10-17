5345caee653ad434b997ab1f617a8d6b64ef6474
Evgenii <wojda007@gmail.com>
2019-10-17 18:33:40 +0300

    Updates

---
+++ precache-manifest.d03cdecdb7030f70623dce539493c151.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38bc8c4b294d2c5850b5dd69de5309d3",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "d3b7d4d1e7dee62988b8",
+    "url": "/goit-react-hw-01-components/static/css/main.dc7e022d.chunk.css"
+  },
+  {
+    "revision": "870e6b8915bed660b43f",
+    "url": "/goit-react-hw-01-components/static/js/2.1b94b6ae.chunk.js"
+  },
+  {
+    "revision": "d3b7d4d1e7dee62988b8",
+    "url": "/goit-react-hw-01-components/static/js/main.7a76d322.chunk.js"
+  },
+  {
+    "revision": "4d9ce36fabe79d32c0ed",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.7a5c0bc9.js"
+  }
+]);
